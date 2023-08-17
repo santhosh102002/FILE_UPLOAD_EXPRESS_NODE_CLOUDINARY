a2e6b8cc2165b99f84e22aad994269875b4fbf5e
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-17 23:43:15 +0530

    Uploading files to Cloudinary

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,5 +1,14 @@
 const file = require('../models/File');
+const cloudinary = require('cloudinary').v2;
 
+const isFileTypeSame = (type,supportedTypes)=>{
+    return supportedTypes.include(type);
+    
+}
+const uploadFileToCloudinary = async (file,folder)=>{
+    const options = {folder};
+    return await cloudinary.uploader.upload(file.tempFilePath,options);
+}
 
 exports.localFileUpload = async(req,res)=>{
     try{
@@ -17,4 +26,42 @@ exports.localFileUpload = async(req,res)=>{
     catch(err){
         console.log(err)
     }
+}
+
+exports.imageUpload = async (req,res)=>{
+    try{
+    const {name,tags,email} = req.body;
+    const file = req.files.imgaeFile;
+
+    // File type validation
+    const supportedTypes = ['jpg','jpeg','png']
+    const fileType = file.name.split('.')[1].toLowerCase();
+    if(!isFileTypeSame(fileType,supportedTypes)){
+        return res.status(400).json({
+            success: false,
+            message: "File format not supported"
+        })
+
+    }
+    const response = await uploadFileToCloudinary(file,"santhosh");
+    console.log(response)
+    
+    // Entry in db
+    // const 
+    res.status(200).json({
+        success: true,
+        message: "Successfully uploaded to cloudinary"
+    })
+
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: "Failed to upload to cloudinary"
+        })
+    }
+
+
 }
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router();
-const {localFileUpload} = require('../controllers/fileUpload')
+const {localFileUpload, imageUpload} = require('../controllers/fileUpload')
 
 router.post('/localFileUpload',localFileUpload)
+router.post('/imageUpload',imageUpload)
 
 module.exports = router
